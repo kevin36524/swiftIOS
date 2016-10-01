acc7c18f217509a170e4cb99d4ed071f8ad9c1f7
Kevin Patel <patelkev@yahoo-inc.com>
2016-10-01 15:56:48 -0700

    Adding Block Operations with multiple blocks

--- NSOperation/NSOperation.playground/Contents.swift
+++ NSOperation/NSOperation.playground/Contents.swift
@@ -20,3 +20,20 @@ summationOperation.start()
 stopClock()
 
 result
+
+//: BlockOperations can have multiple blocks, that run concurrently.
+//: Cannot guarantee the order in which the operations will be executed.
+//: Number of concurrent blocks that can be executed depends on the number of available threads.
+//: Add some more blocks and observe the time taken.
+
+let multiPrinter = BlockOperation()
+multiPrinter.addExecutionBlock { print ("All"); sleep(5);}
+multiPrinter.addExecutionBlock { print ("these"); sleep(5);}
+multiPrinter.addExecutionBlock { print ("blocks"); sleep(5);}
+multiPrinter.addExecutionBlock { print ("will"); sleep(5);}
+multiPrinter.addExecutionBlock { print ("run"); sleep(5);}
+multiPrinter.addExecutionBlock { print ("concurrently"); sleep(5);}
+
+startClock()
+multiPrinter.start()
+stopClock()
